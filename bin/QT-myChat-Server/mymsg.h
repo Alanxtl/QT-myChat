71d834b11523cde7e0729c23dc0adfea3fa5e8bc
Xuetao Li <xuetaoli@bit.edu.cn>
2023-08-24 22:42:12 +0800

    定义了msg类

--- bin/QT-myChat-Server/mymsg.h
+++ bin/QT-myChat-Server/mymsg.h
@@ -5,19 +5,19 @@
 #define identify 0xAA112233
 
 /*
- * 标准QByteArray信息格式|header|body|
+ * 标准QByteArray信息格式|32header|?body|
  *
  * header总共32字节包括以下内容:
- *     dataSize    : 占 4 个字节, quint32, 用于验证是否完整接收到了数据包
- *     identify    : 占 4 个字节, quint32, 用于验证是不是该程序的数据包，是一个默认的常量
- *     type        : 占 1 个字节, quint8, 用于标识数据包类型
- *          数据包类型及代码如下：0-注册信息 1-登录信息 2-普通信息 3-图片信息 4-文件信息
- *     slice       : 占 1 个字节，quint8，用以指示该信息是否为切片信息，若非切片信息以下两条数据应为0
- *     sliceTotal  : 占 4 个字节, quint32, 用以指示切片信息切片总数
- *     sliceCount  : 占 4 个字节, quint32, 用以指示该切片信息是第几个切片
- *     senderID    : 占 4 个字节, quint32, 用于表示发送者的id
- *     receiverID  : 占 4 个字节, quint32, 用于表示接收者的id
- *     time        : 占 4 个字节, quint32, 用于表示发送的时间
+ *          dataSize    : 占 4 个字节, quint32, 用于验证是否完整接收到了数据包
+ *          identify    : 占 4 个字节, quint32, 用于验证是不是该程序的数据包，是一个默认的常量
+ *          type        : 占 1 个字节, quint8, 用于标识数据包类型
+ *                 数据包类型及代码如下：0-注册信息 1-登录信息 2-普通信息 3-图片信息 4-文件信息
+ *  不一定做 slice       : 占 1 个字节，quint8，用以指示该信息是否为切片信息，若非切片信息以下两条数据应为0
+ *  不一定做 sliceTotal  : 占 4 个字节, quint32, 用以指示切片信息切片总数
+ *  不一定做 sliceCount  : 占 4 个字节, quint32, 用以指示该切片信息是第几个切片
+ *          senderID    : 占 4 个字节, quint32, 用于表示发送者的id
+ *          receiverID  : 占 4 个字节, quint32, 用于表示接收者的id
+ *          time        : 占 4 个字节, quint32, 用于表示发送的时间
  *
  *
  *
@@ -42,8 +42,17 @@ class myMsg : public QObject
     Q_OBJECT
 public:
     explicit myMsg(QObject *parent = nullptr);
+    myMsg(quint8 type, const QByteArray & content);
+
+    // |32header|?body|
+    quint32 dataSize;
+    quint8 type;
+    QByteArray content;
+    QByteArray header;
 
 signals:
+
+
 
 };
 
