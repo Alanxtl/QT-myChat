683137236c777c25f196472ad444edcfa1709293
xiaozhe <1106775067@qq.com>
2023-08-28 15:50:09 +0800

    客户端数据库接口完善

--- bin/QT-myChat-Client/Database/DBHelper.h
+++ bin/QT-myChat-Client/Database/DBHelper.h
@@ -23,50 +23,17 @@ class DBHelper : public QObject
 public:
     explicit DBHelper(QObject *parent = nullptr);
     ~DBHelper();
-    //登录功能数据库
-    bool selectUserByIdAndPwd(const QString username, const QString pwd);//存在返回true，不存在返回false
+    //查找好友
     UserInfo selectUserInfoById(const quint32 id);//存在返回true，不存在返回false
-
-    //查询当前全部用户的数据库操作--静态sql
-    QList<QByteArray> selectAllFriendsUserInfo(quint32 UserId);
-    //注册
-    bool registerUserInfo(const UserInfo& user);//
+    //查询好友列表
+    QList<QByteArray> selectAllFriendsUserInfo();
     //添加好友
-    bool addFriendship(quint32 Id1,quint32 Id2);//两个好友的Id
+    bool addFriendship(quint32 Id, QString name);//两个好友的Id
     //删除好友
-    bool deleteFriendship(quint32 Id1,quint32 Id2);//两个好友的Id
-    //判断好友关系是否存在
-    bool friendshipExist(quint32 Id1,quint32 Id2);
-    //添加聊天信息
-    bool addMsg(quint32 Id1,quint32 Id2,QString Msg,QString Date);//发送者的Id，接收者的Id，聊天信息，标准日期xxxx-xx-xx xx:xx:xx使用库获取日期
-    //获取最大的Id值
-    quint32 selectMaxId();
+    bool deleteFriendship(quint32 Id);//两个好友的Id
 
     //单例设计模式--应对数据库的对象的内存使用
     static DBHelper* GetInstance();
-
-    //处理离线消息
-    //添加离线消息
-    bool addOfflineMsg(ChatMessage &msg);
-    //获取离线消息
-    QList<ChatMessage> getOfflineMsg(quint32 ID);
-    //删除离线消息
-    bool dropOfflineMsg(quint32 ID);
-
-    //群组操作
-    //添加群聊关系
-    bool addGroupship(quint32 UserID,quint32 GroupID,quint32 UserPermission = 0);
-    bool registerGroupInfo(const GroupInfo& Group);
-    quint32 selectMaxGroupId();
-    GroupInfo selectGroupInfoByID(quint32);
-    // Id1用户ID,Id2群聊ID,返回Id1是否在Id2中
-    bool groupshipExist(quint32 Id1, quint32 Id2);
-    QList<QByteArray> selectAllGroupInfo(quint32 ID);
-    QList<quint32> selectAllGroupMember(quint32 ID);
-
-    //修改用户名和头像
-    bool updUsername(quint32 ID,QString Username);
-    bool updAvatar(quint32 ID,QString Avatar);
 
 private:
     QSqlDatabase sqldb;
--- bin/QT-myChat-Client/QT-myChat-Client.pro
+++ bin/QT-myChat-Client/QT-myChat-Client.pro
@@ -1,4 +1,5 @@
 QT       += core gui sql
+QT       += network
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -9,6 +10,10 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    Database/ChatMessage.cpp \
+    Database/DBHelper.cpp \
+    Database/GroupInfo.cpp \
+    Database/UserInfo.cpp \
     Tools/mymsg.cpp \
     chapage.cpp \
     login.cpp \
@@ -18,6 +23,11 @@ SOURCES += \
     regis.cpp
 
 HEADERS += \
+    Database/ChatMessage.h \
+    Database/DBHelper.h \
+    Database/GroupInfo.h \
+    Database/UserInfo.h \
+    Database/standarddatetimeutilities.h \
     Tools/mymsg.h \
     chapage.h \
     login.h \
